e572409ab764ce12b964bab5dadd6b3e2c52bda4
Raymark12345 <135635966+Raymark12345@users.noreply.github.com>
2023-06-05 23:35:27 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+
+
+
+
+var typed = new Type(".multiple-text") {
+  strings:["Frontend Developer", "Student", "WR Player", "YouTuber", "Programmer"], 
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+};
